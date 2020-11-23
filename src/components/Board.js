7753f063b77c6aa2ee8411e48fd409f71ab4e9f3
shubham98 <shubhambhojane90@gmail.com>
2020-11-23 15:33:36 +0530

    added board state and also react event

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Square from './Square'
 
 
 const Board = () => {
+
+    const [board , setBoard] = useState(Array(9).fill(null));
+    const [isXnext,setIsXNext]  = useState(false);
+    // handle logic of onclick event
+    const handleSquareClick = (position)=> {
+
+            if(board[position]){
+                return;
+            }    
+        setBoard(prev =>{
+            return prev.map((square,pos) => {
+                if(pos === position){
+                    return isXnext ? 'X' : 'O';
+                }
+                return square;
+            });
+        });
+        setIsXNext((prev) => !prev);
+    }; 
+    
+ // custom function for current position
+    const renderSquare = position => {
+        return (<Square value = {board[position]} 
+            onClick={() => handleSquareClick(position)}
+            />
+            );
+    };
+
     return (
-        <div>
+        <div className="board">
             
-           <div>
-               <Square value = {2} />
-               <Square value = {3} />
-               <Square value = {4} />
+           <div className="board-row">
+               {renderSquare(0)}
+               {renderSquare(1)}
+               {renderSquare(2)}
             </div>
-           <div>
-               <Square value = {5} />
-               <Square value = {6} />
-               <Square value = {7} />
+           <div className="board-row">
+           {renderSquare(3)}
+           {renderSquare(4)}
+           {renderSquare(5)}
             </div>
-           <div>
-               <Square value = {8} />
-               <Square value = {9} />
-               <Square value = {10} />
+           <div className="board-row">
+           {renderSquare(6)}
+           {renderSquare(7)}
+           {renderSquare(8)}
                
             </div>
         </div>
